f6a8c798fd370cb8f31ea6aa455903beba3c315c
Bossnexx <johnxyliernacilla@gmail.com>
2024-08-12 05:32:14 +0800

    Update Mainx.py

--- Mainx.py
+++ Mainx.py
@@ -12,7 +12,7 @@ from rich.console import Console
 from rich.prompt import Prompt, IntPrompt
 from rich.text import Text
 from rich.style import Style
-from cpmewan import CPMEwan
+from cpmnex import CPMNex
 
 __CHANNEL_USERNAME__ = "Roasted2001"
 __GROUP_USERNAME__   = "0983544223"
@@ -118,7 +118,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = cpmrst(acc_access_key)
+        cpm = cpmnex(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
